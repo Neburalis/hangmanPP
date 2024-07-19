bb83cdea0f84c980fb9f30d8e4c9861a79bd42c2
senymama <senymama228@gmail.com>
2024-07-19 22:45:58 +0300

    Application commit The application is ready, commit to send for refactoring

---
+++ main.py
@@ -0,0 +1,68 @@
+import random
+from rows import *
+
+words = []
+with open("words.txt", 'r') as file:
+    for line in file:
+        words.append(line.strip())
+
+
+def mask_generate(word: str) -> str:
+    mask = ''
+    for letter in word:
+        if letter.lower() in answers:
+            mask += f'{letter} '
+        else:
+            mask += '_ '
+    return mask.strip()
+
+
+def all_letters_in_set(word: str, st: set) -> bool:
+    for letter in word:
+        if not letter.lower() in st:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    game = True
+
+    while game:
+        print(start_message)
+        start_command = input()
+        if start_command.strip().lower() == "n":
+            game = True
+        elif start_command.strip().lower() == "e":
+            game = False
+            break
+        else:
+            continue
+        word = random.choice(words)
+        # print(word)
+        answers = set()
+        mask = mask_generate(word)
+        stage = 0
+        tries = 0
+
+        while stage < len(gallows_stages)-1:
+            print(gallows_stages[stage].format(mask, answers))
+            letter = input()
+            tries += 1
+            if letter.lower() == word:
+                print(won_message.format(word, tries))
+                break
+            elif len(letter) == 1:
+                answers.add(letter)
+                mask = mask_generate(word)
+                if all_letters_in_set(word, answers):
+                    print(won_message.format(word, tries))
+                    break
+                else:
+                    if letter in word:
+                        pass
+                    else:
+                        stage += 1
+        else:
+            print(gallows_stages[stage].format(mask, answers))
+            print(lost_message.format(word))
+
