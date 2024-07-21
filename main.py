59f27257dba996d70ef6553391cdcd68598f890a
senymama <senymama228@gmail.com>
2024-07-21 07:58:08 +0300

    Refactoring of main.py - The style has been changed from vague to functional

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import random
 from rows import *
 
-words = []
-with open("words.txt", 'r') as file:
-    for line in file:
-        words.append(line.strip())
+
+def get_words_from_file(filename):
+    """
+
+    :param filename: filename from which the word base will be obtained
+    :return: words base in list
+    """
+    words = []
+    with open(filename, 'r') as file:
+        for line in file:
+            words.append(line.strip().lower())
+    return words
 
 
-def mask_generate(word: str) -> str:
+def get_words_from_other():
+    """
+    another implementation of getting words
+    :return:
+    """
+    ...
+
+
+def mask_generate(word: str, answers: set) -> str:
     mask = ''
     for letter in word:
         if letter.lower() in answers:
@@ -24,47 +40,52 @@ def all_letters_in_set(word: str, st: set) -> bool:
     return True
 
 
-if __name__ == '__main__':
-    game = True
+def game(word: str):
+    # print(word)
+    answers = set()
+    mask = mask_generate(word, answers)
+    stage = 0
+    tries = 0
 
-    while game:
+    while stage < len(gallows_stages) - 1:
+        print(gallows_stages[stage].format(mask, answers))
+        letter = input().strip().lower()
+        tries += 1
+        if letter == word:
+            print(won_message.format(word, tries))
+            break
+        elif len(letter) == 1:
+            if letter in answers:
+                tries -= 1
+            else:
+                answers.add(letter)
+                mask = mask_generate(word, answers)
+                if all_letters_in_set(word, answers):
+                    print(won_message.format(word, tries))
+                    break
+                else:
+                    if letter in word:
+                        pass
+                    else:
+                        stage += 1
+    else:
+        print(gallows_stages[stage].format(mask, answers))
+        print(lost_message.format(word))
+
+
+def main():
+    words = get_words_from_file("words.txt")
+    while True:
         print(start_message)
-        start_command = input()
-        if start_command.strip().lower() == "n":
-            game = True
-        elif start_command.strip().lower() == "e":
-            game = False
+        start_command = input().strip().lower()
+        if start_command == "n":
+            word = random.choice(words)
+            game(word)
+        elif start_command == "e":
             break
         else:
             continue
-        word = random.choice(words).strip().lower()  # in case the words data is crooked
-        # print(word)
-        answers = set()
-        mask = mask_generate(word)
-        stage = 0
-        tries = 0
 
-        while stage < len(gallows_stages) - 1:
-            print(gallows_stages[stage].format(mask, answers))
-            letter = input().strip().lower()
-            tries += 1
-            if letter == word:
-                print(won_message.format(word, tries))
-                break
-            elif len(letter) == 1:
-                if letter in answers:  # If you enter the same letter again, it will not be counted as an error.
-                    pass
-                else:
-                    answers.add(letter)
-                    mask = mask_generate(word)
-                    if all_letters_in_set(word, answers):
-                        print(won_message.format(word, tries))
-                        break
-                    else:
-                        if letter in word:
-                            pass
-                        else:
-                            stage += 1
-        else:
-            print(gallows_stages[stage].format(mask, answers))
-            print(lost_message.format(word))
+
+if __name__ == '__main__':
+    main()
