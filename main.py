72d0b75edf09dc55e7afcd75107de178f97020e5
senymama <senymama228@gmail.com>
2024-07-21 08:33:52 +0300

    Refactoring of main.py - Some actions are additionally taken out into functions - Added saving the result to a file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rows import *
 
 def get_words_from_file(filename):
     """
-
+    function to get the word base for the game from a file
     :param filename: filename from which the word base will be obtained
     :return: words base in list
     """
@@ -24,6 +24,12 @@ def get_words_from_other():
 
 
 def mask_generate(word: str, answers: set) -> str:
+    """
+    function for generating a word mask - a string in which unknown letters are hidden
+    :param word: word for generating mask
+    :param answers: set of known letters
+    :return: generated mask
+    """
     mask = ''
     for letter in word:
         if letter.lower() in answers:
@@ -34,14 +40,33 @@ def mask_generate(word: str, answers: set) -> str:
 
 
 def all_letters_in_set(word: str, st: set) -> bool:
+    """
+    function for checking if all letters in the word are present in the set
+    :param word: word for checking
+    :param st: set of known letters
+    :return:
+    """
     for letter in word:
         if not letter.lower() in st:
             return False
     return True
 
 
-def game(word: str):
-    # print(word)
+def send_won_message(word, tries) -> None:
+    print(won_message.format(word, tries))
+
+
+def send_lost_message(word) -> None:
+    print(lost_message.format(word))
+
+
+def game(word: str) -> bool:
+    """
+    game main function
+    :param word: hidden word
+    :return: True if game won, False otherwise
+    """
+    print(word)
     answers = set()
     mask = mask_generate(word, answers)
     stage = 0
@@ -61,26 +86,62 @@ def game(word: str):
                 answers.add(letter)
                 mask = mask_generate(word, answers)
                 if all_letters_in_set(word, answers):
-                    print(won_message.format(word, tries))
-                    break
+                    send_won_message(word, tries)
+                    return True
                 else:
                     if letter in word:
                         pass
                     else:
                         stage += 1
-    else:
-        print(gallows_stages[stage].format(mask, answers))
-        print(lost_message.format(word))
+    print(gallows_stages[stage].format(mask, answers))
+    send_lost_message(word)
+    return False
 
 
-def main():
+def save_results(result: bool, player_name: str, filename: str) -> None:
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+        for i, line in enumerate(lines):
+            name, wins, losses = line.strip().split()
+            if name == player_name:
+                wins = int(wins)
+                losses = int(losses)
+                if result:
+                    wins += 1
+                else:
+                    losses += 1
+                lines[i] = f'{name} {wins} {losses}\n'
+                break
+        else:
+            if result:
+                lines.append(f'{player_name} 1 0\n')
+            else:
+                lines.append(f'{player_name} 0 1\n')
+    except FileNotFoundError:
+        with open(filename, 'w') as file:
+            if result:
+                file.write(f'{player_name} 1 0\n')
+            else:
+                file.write(f'{player_name} 0 1\n')
+
+
+def main() -> None:
+    """
+    game launch function
+    :return: None
+    """
     words = get_words_from_file("words.txt")
     while True:
         print(start_message)
         start_command = input().strip().lower()
         if start_command == "n":
+            print('What is your name?', 'This is necessary for compiling statistics')
+            name = input().strip()
             word = random.choice(words)
-            game(word)
+            result = game(word)
+            save_results(result, name, "results.txt")
         elif start_command == "e":
             break
         else:
